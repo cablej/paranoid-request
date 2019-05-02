33b7b4e1b22757d5e421c31f8217acd0687765bb
Jack Cable <jackhcable@gmail.com>
2019-05-01 19:35:54 -0700

    Update to latest Node version
    See https://github.com/evert/paranoid-request/tree/normalize-args

--- src/_wrapper_shared.js
+++ src/_wrapper_shared.js
@@ -156,7 +156,9 @@ function safeConnectionWrapper(fn, wrappingSafeConnect) {
   wrappingSafeConnect = (wrappingSafeConnect || false);
 
   return function safeConnectionWrappedFn() { // eslint-disable-line max-statements
-    const args = net._normalizeArgs(arguments);
+    const normalizeArgs = net._normalizeArgs ? net._normalizeArgs : net._normalizeConnectArgs;
+
+    const args = normalizeArgs(arguments);
     const options = args[0];
 
     // We smuggled our validator through localAddress
